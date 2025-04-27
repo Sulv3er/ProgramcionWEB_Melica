945eafddcfc7ccf72742405b2c4fd875a9907853
DulceRoque04 <road030904@itsuruapan.edu.mx>
2025-04-27 10:14:00 -0600

    se eliminaron comentarios

--- model/model.php
+++ model/model.php
@@ -1,6 +1,6 @@
 <?php
 $host = "localhost"; // Servidor de la base de datos
-$dbname = "pruebaPablo"; // Nombre de tu base de datos
+$dbname = "pruebaPablo"; // Nombre base de datos
 $username = "root"; // Usuario de la base de datos
 $password = ""; // Contraseña del usuario
 
@@ -8,7 +8,7 @@ try {
     $conexion = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     // Configurar el modo de error de PDO a excepción
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Conexión exitosa"; // Puedes comentar esto después de probar
+    // echo "Conexión exitosa"; 
 } catch(PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
